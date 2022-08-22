3a409e517160feaf06a123fa02fd96588b589863
ngodingbentar <dewaqintoro@gmail.com>
2022-08-22 22:51:47 +0700

    initial

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <RichTextEditorVue />
+  </div>
+</template>
+
+<script>
+import RichTextEditorVue from './components/RichTextEditor.vue';
+export default {
+  name: 'App',
+  components: {
+    RichTextEditorVue,
+  }
+}
+</script>
+
+<style>
+#app {
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/RichTextEditor.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <QuillEditor v-model:content="content" contentType="html" theme="snow" toolbar="full" />
+    <div>
+      <button @click="saveData">save</button>
+    </div>
+    <div>
+      <div v-for="item in data" :key="item.id">
+        <h3>{{item.title}}</h3>
+        <div v-html="item.body"></div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import { QuillEditor } from '@vueup/vue-quill'
+import '@vueup/vue-quill/dist/vue-quill.snow.css';
+import '@vueup/vue-quill/dist/vue-quill.bubble.css';
+
+export default {
+  components: {
+    QuillEditor
+  },
+  data() {
+    return {
+      content: 'Test',
+      data: [
+        {
+          id: 1,
+          title: 'title 1',
+          body: 'body 1'
+        },
+        {
+          id: 2,
+          title: 'title 2',
+          body: 'body 2'
+        }
+      ]
+    }
+  },
+  methods: {
+    saveData() {
+      this.data.push({
+        id: this.data.length + 1,
+        title: 'Title ' + this.data.length,
+        body: this.content
+      })
+    }
+  }
+}
+</script>
